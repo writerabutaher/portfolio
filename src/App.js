1d67dc607ee7b326374794094b1813dabaefdb3c
Abu Taher <writerabutaher@gmail.com>
2022-12-11 16:39:47 +0600

    project start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen animate-bounce">
+        <h1 className="text-5xl">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
